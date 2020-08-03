cfc81d62a281f68f81af012fc366018f9446f6ed
primepasta <69119304+primepasta@users.noreply.github.com>
2020-08-03 18:17:00 +0300

    Updating files.

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ setup(
   description = 'Helps in deploying django files to heroku.',   # Give a short description about your library
   author = 'primepasta',                   # Type in your name
   author_email = 'theprimepasta@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  url = 'https://github.com/primepasta/testing-primepasta',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/primepasta/testing-primepasta/archive/v_01.tar.gz',    # I explain this later on
   keywords = ['django', 'heroku', 'deploy'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'subprocess',
