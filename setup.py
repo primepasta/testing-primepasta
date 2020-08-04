0f23952409ea4196edb032eb6ed0053c925eb44f
primepasta <69119304+primepasta@users.noreply.github.com>
2020-08-04 12:39:31 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,9 +11,7 @@ setup(
   download_url = 'https://github.com/primepasta/testing-primepasta/archive/v_02.tar.gz',    # I explain this later on
   keywords = ['django', 'heroku', 'deploy'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'subprocess',
-          'keyboard',
-          'time',
+          'twine',
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
